189492a23c250ba030e548d39b328abc06835446
rjm <robjmass@gmail.com>
2024-06-04 16:06:07 -0400

    unnamed commit

--- supermarket/src/App.js
+++ supermarket/src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+let fetchData = async () => {
+    let stockSymbol = document.querySelector('#stockInput').value;
+    // let apikey = read(); 
+    let apikey = "key";
+
+    let api = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockSymbol}&apikey=${apikey}`;
+
+    let res = await fetch(api);
+    let data = await res.json();
+    displayData(data);
+    console.log('fetching');
+};
+
+let displayData = (data) => {
+    let stockDataset = [];
+    let labels = [];
+    console.log(data);
+
+    for (let date in data['Time Series (Daily)']) {
+        console.log(date);
+        stockDataset.unshift(parseFloat(data['Time Series (Daily)'][date]['4. close']));
+        labels.unshift(date);
+    }
+
+    let ctx = document.querySelector('#chart').getContext('2d');
+    const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Stock Price',
+                data: stockDataset,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+            }]
+        }
+    });
+};
+
 function App() {
+    
   return (
     <div className="App">
       <header className="App-header">
