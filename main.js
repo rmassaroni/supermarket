7329641a523141fb8cba2dcc15666c00f808491d
rjm <robjmass@gmail.com>
2024-06-04 15:56:30 -0400

    react setup

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
+
+
 let fetchData = async () => {
     let stockSymbol = document.querySelector('#stockInput').value;
-    let apikey = 'YOUR_API_KEY'; 
+    let apikey = read(); 
 
     let api = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${stockSymbol}&apikey=${apikey}`;
 
     let res = await fetch(api);
     let data = await res.json();
     displayData(data);
+    console.log('fetching');
 };
 
 let displayData = (data) => {
     let stockDataset = [];
     let labels = [];
+    console.log(data);
 
     for (let date in data['Time Series (Daily)']) {
+        console.log(date);
         stockDataset.unshift(parseFloat(data['Time Series (Daily)'][date]['4. close']));
         labels.unshift(date);
     }
@@ -32,3 +37,16 @@ let displayData = (data) => {
         }
     });
 };
+
+let read = () => {
+    const fs = require('fs');
+    var key;
+    fs.readFile('private/alpha-vantage-key.txt', (err, data) => {
+        if (err) throw err;
+
+        console.log(data.toString());
+        key = data;
+    });
+    return key;
+};
+
